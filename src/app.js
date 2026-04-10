fbfd717d0e44ce93565e3238d94e6b43a2704ed1
GAEL JONAS ZOLOTOW <49379479@ort>
2026-04-10 08:16:05 -0300

    terminadoo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const obtenerHoraActual = require("./time");
+import express from "express";
+import { obtenerHoraActual } from "./time.js";
 
 const app = express();
 
